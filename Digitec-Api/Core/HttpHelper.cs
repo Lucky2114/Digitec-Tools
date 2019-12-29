566a7bdb235a4d628bf7ab50bd101fec6c8a1324
Kevin Müller <kevin.mueller1@outlook.com>
2019-12-29 11:13:49 +0100

    Added API Methods

--- Digitec-Api/Core/HttpHelper.cs
+++ Digitec-Api/Core/HttpHelper.cs
@@ -1,11 +1,19 @@
-﻿using System;
+﻿using HtmlAgilityPack;
+using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace Digitec_Api.Core
 {
     internal class HttpHelper
     {
         //TODO Add downloading (and parsing?) methods here.
+        internal static async Task<HtmlDocument> GetDocument(string url)
+        {
+            //url has to be tested here
+            var web = new HtmlWeb();
+            return await web.LoadFromWebAsync(url);
+        }
     }
 }
