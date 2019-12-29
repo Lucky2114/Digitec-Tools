5a8552b95bb781843d55ebf18161d7fb9d6df745
Kevin Müller <kevin.mueller1@outlook.com>
2019-12-29 16:10:12 +0100

    Updated Web Project

---
+++ Digitec-Api/Config/DigitecWebConstatnts.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Digitec_Api.Config
+{
+    internal static class DigitecWebConstatnts
+    {
+        internal static string[] ProductElementClasses = { "productName", "ZZej" };
+    }
+}
--- Digitec-Api/Core/Digitec.cs
+++ Digitec-Api/Core/Digitec.cs
@@ -1,15 +1,18 @@
-﻿using Digitec_Api.Core;
+﻿using Digitec_Api.Config;
+using Digitec_Api.Core;
 using System;
+using System.Linq;
 
 namespace Digitec_Api
 {
     public static class Digitec
     {
-        public static void GetProductInfo(string productUrl)
+        public static async void GetProductInfo(string productUrl)
         {
             if (Validation.IsValidDigitecUrl(productUrl))
             {
-
+                var doc = await HttpHelper.GetDocument(productUrl);
+                var x = doc.DocumentNode.Descendants().Where(x => x.GetClasses().SequenceEqual(DigitecWebConstatnts.ProductElementClasses));
             }
         }
     }
