9923c6eec3330af4913d42f843b65829a170070c
Kevin Müller <kevin.mueller1@outlook.com>
2020-01-07 14:27:19 +0100

    Removed Identity Scaffolding

--- Digitec-Tools-Web/Areas/Identity/Pages/Account/LogOut.cshtml.cs
+++ Digitec-Tools-Web/Areas/Identity/Pages/Account/LogOut.cshtml.cs
@@ -1,18 +1,16 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace Digitec_Tools_Web.Areas.Identity.Pages.Account
 {
-    [AllowAnonymous]
-    public class ResetPasswordConfirmationModel : PageModel
+    public class LogOutModel : PageModel
     {
         public void OnGet()
         {
-
         }
     }
 }
