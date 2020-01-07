b14fa3f707d28ab20ac10ca21e759080c0b623f2
Kevin Müller <kevin.mueller1@outlook.com>
2020-01-07 13:09:14 +0100

    Added Identity Scaffolding

---
+++ Digitec-Tools-Web/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Digitec_Tools_Web.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(Digitec_Tools_Web.Areas.Identity.IdentityHostingStartup))]
+namespace Digitec_Tools_Web.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
