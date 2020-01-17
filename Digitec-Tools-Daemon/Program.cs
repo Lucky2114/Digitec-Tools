36060bc6b0a027ff0caa964c40638db9bd292a19
Kevin Müller <kevin.mueller1@outlook.com>
2020-01-17 10:07:19 +0100

    More logging

--- Digitec-Tools-Daemon/Program.cs
+++ Digitec-Tools-Daemon/Program.cs
@@ -11,17 +11,23 @@ namespace Digitec_Tools_Daemon
         static void Main(string[] args)
         {
             //This Daemon just runns the tasks from the class library
-
-            List<ITask> tasks = new List<ITask>()
+            try
+            {
+                List<ITask> tasks = new List<ITask>()
             {
                 //Update every 10 minutes.
                 new ChangesToPriceTask(10)
             };
 
-            foreach (ITask task in tasks)
+                foreach (ITask task in tasks)
+                {
+                    Console.WriteLine("Starting Task..");
+                    task.StartTask();
+                }
+            } catch (Exception ex)
             {
-                Console.WriteLine("Starting Task..");
-                task.StartTask();
+                Console.WriteLine("Exception! ");
+                Console.WriteLine(ex.Message);
             }
 
             Console.ReadLine();
