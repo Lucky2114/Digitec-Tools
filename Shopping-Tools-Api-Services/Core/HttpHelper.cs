d84d70bf419cc8d6f59b05c696a43c1eefbcaa42
Kevin Müller <kevin.mueller1@gmx.ch>
2020-02-08 16:12:44 +0100

    another attempt on bypassing server cache

--- Shopping-Tools-Api-Services/Core/HttpHelper.cs
+++ Shopping-Tools-Api-Services/Core/HttpHelper.cs
@@ -1,5 +1,9 @@
 ﻿using HtmlAgilityPack;
+using Newtonsoft.Json;
+using Shopping_Tools_Api_Services.Models;
 using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Net;
 using System.Net.Cache;
 using System.Threading.Tasks;
@@ -8,35 +12,73 @@ namespace Shopping_Tools_Api_Services.Core
 {
     internal static class HttpHelper
     {
+        //TODO Create list of user agents to rotate
+        //TOOD Create list of proxys to use
+        private static readonly List<WebProxy> _workingProxies = new List<WebProxy>();
+
         private const string _userAgent = "Mozilla/5.0 (Windows; U; Windows NT 5.1; de-DE; rv:x.x.x) Gecko/20041107 Firefox/x.x";
+
+        private static bool _proxyLimitReached;
 
         internal static async Task<HtmlDocument> GetDocument(string url)
         {
-            Uri uri = new Uri(url);
-            var req = WebRequest.Create(uri) as HttpWebRequest;
+            //Uri uri = new Uri(url);
+            //var req = WebRequest.Create(uri) as HttpWebRequest;
 
-            // TODO:
-            // Not sure if this bypasses the server cache.
-            var environmentProxy = Environment.GetEnvironmentVariable("HTTP_PROXY", EnvironmentVariableTarget.User);
+            //req.Proxy = GetRandomProxy();
+            //req.UserAgent = _userAgent;
+            //req.Method = "GET";
+            //req.CachePolicy = new RequestCachePolicy(RequestCacheLevel.NoCacheNoStore);
+            //var source = await req.GetResponseAsync();
+            //var stream = source.GetResponseStream();
 
-            if (!string.IsNullOrEmpty(environmentProxy))
+            //var doc = new HtmlDocument();
+            //doc.Load(stream, true);
+            //return await Task.FromResult(doc);
+
+            var web = new HtmlWeb();
+            return await web.LoadFromWebAsync(url);
+        }
+
+        internal static WebProxy GetRandomProxy()
+        {
+            var client = new WebClient();
+            var result = client.DownloadString(new Uri("https://api.getproxylist.com/proxy?protocol[]=http&allowsHttps=1"));
+
+            WebProxy webProxy = null;
+            try
             {
-                req.Proxy = new WebProxy
+                RestProxyResult proxy = JsonConvert.DeserializeObject<RestProxyResult>(result);
+                if (proxy != null)
                 {
-                    Address = new Uri(environmentProxy),
-                    UseDefaultCredentials = true
-                };
-                req.UseDefaultCredentials = true;
-            }
-            req.UserAgent = _userAgent;
-            req.Method = "GET";
-            req.CachePolicy = new RequestCachePolicy(RequestCacheLevel.NoCacheNoStore);
-            var source = await req.GetResponseAsync();
-            var stream = source.GetResponseStream();
+                    if (_proxyLimitReached)
+                    {
+                        //A new day. Another 25 requests at our disposal.
+                        //flush the old proxies (they could be out of date)
+                        _workingProxies.Clear();
+                        _proxyLimitReached = false;
+                    }
 
-            var doc = new HtmlDocument();
-            doc.Load(stream, true);
-            return await Task.FromResult(doc);
+                    Console.WriteLine($"Using proxy in {proxy.country}");
+                    webProxy = new WebProxy()
+                    {
+                        Address = new Uri($"http://{proxy.ip}:{proxy.port}")
+                    };
+                    if (!_workingProxies.Any(x => x.Address.Equals(webProxy.Address)))
+                        _workingProxies.Add(webProxy);
+                }
+            }
+            catch
+            {
+                Console.WriteLine($"Proxy limit is reached! Currenty {_workingProxies.Count} proxies cached.");
+                _proxyLimitReached = true;
+                //Limit reached
+                //Return a random proxy from the list
+                if (_workingProxies.Count > 0)
+                    webProxy = _workingProxies[new Random().Next(_workingProxies.Count)];
+            }
+
+            return webProxy;
         }
     }
 }
---
+++ Shopping-Tools-Api-Services/Models/RestProxyResult.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Shopping_Tools_Api_Services.Models
+{
+
+    public class RestProxyResult
+    {
+        public _Links _links { get; set; }
+        public string ip { get; set; }
+        public int port { get; set; }
+        public string protocol { get; set; }
+        public string anonymity { get; set; }
+        public string lastTested { get; set; }
+        public bool allowsRefererHeader { get; set; }
+        public bool allowsUserAgentHeader { get; set; }
+        public bool allowsCustomHeaders { get; set; }
+        public bool allowsCookies { get; set; }
+        public bool allowsPost { get; set; }
+        public bool allowsHttps { get; set; }
+        public string country { get; set; }
+        public string connectTime { get; set; }
+        public string downloadSpeed { get; set; }
+        public string secondsToFirstByte { get; set; }
+        public string uptime { get; set; }
+    }
+
+    public class _Links
+    {
+        public string _self { get; set; }
+        public string _parent { get; set; }
+    }
+
+}
