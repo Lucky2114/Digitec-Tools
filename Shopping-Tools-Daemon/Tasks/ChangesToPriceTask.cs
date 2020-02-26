54a61c19410d997372b72b6fe7b3b8147f0665ea
Kevin Müller <kevin.mueller1@outlook.com>
2020-02-26 08:53:00 +0100

    attempt to fix random thread crashing

--- Shopping-Tools-Daemon/Tasks/ChangesToPriceTask.cs
+++ Shopping-Tools-Daemon/Tasks/ChangesToPriceTask.cs
@@ -40,85 +40,94 @@ namespace Shopping_Tools_Daemon.Tasks
             var storage = new Storage();
             while (!shouldAbort)
             {
-                Random rand = new Random();
-                var randSleep = rand.Next(25000, 60000);
-                var timer = new TimerPlus()
+                try
                 {
-                    AutoReset = false,
-                    Interval = TimeSpan.FromMinutes(_interval).TotalMilliseconds + randSleep
-                };
-                timer.Start();
-
-                var result = storage.GetAllProducts().Result;
-                Console.WriteLine($"Received {result.Count} products.");
-
-                List<Task> pendingTasks = new List<Task>();
-
-                foreach (var item in result)
-                {
-                    Task task = new Task(delegate
+                    Random rand = new Random();
+                    var randSleep = rand.Next(25000, 60000);
+                    var timer = new TimerPlus()
                     {
-                        //TODO If no users => continue;
-                        var productFromDB = Helpers.DictionaryToProduct(item);
-                        var productCurrent = storage.UpdateProduct(productFromDB).Result;
+                        AutoReset = false,
+                        Interval = TimeSpan.FromMinutes(_interval).TotalMilliseconds + randSleep
+                    };
+                    timer.Start();
 
-                        double priceDB = productFromDB.PriceCurrent.ParseToDouble();
-                        double priceCurrent = productCurrent.PriceCurrent.ParseToDouble();
+                    var result = storage.GetAllProducts().Result;
+                    Console.WriteLine($"Received {result.Count} products.");
 
-                        if (priceCurrent != priceDB)
+                    List<Task> pendingTasks = new List<Task>();
+
+                    foreach (var item in result)
+                    {
+                        Task task = new Task(delegate
                         {
-                            var message =
-                                $"{productCurrent.Brand} {productCurrent.Name} now costs {productCurrent.PriceCurrent} {productCurrent.Currency} instead of {productFromDB.PriceCurrent} {productFromDB.Currency}! \n" +
-                                "\n" +
-                                $"Here's the link: {productCurrent.Url}" +
-                                $"\n\n" +
-                                $"Edit Account Settings: https://www.shoppingtools.online/Identity/Account/Manage";
+                            //TODO If no users => continue;
+                            var productFromDB = Helpers.DictionaryToProduct(item);
+                            var productCurrent = storage.UpdateProduct(productFromDB).Result;
 
-                            Console.WriteLine(message);
-                            Console.WriteLine("Notifying Users...");
-                            try
+                            double priceDB = productFromDB.PriceCurrent.ParseToDouble();
+                            double priceCurrent = productCurrent.PriceCurrent.ParseToDouble();
+
+                            if (priceCurrent != priceDB)
                             {
-                                var notifiedUsers = UserNotifier.NotifyUsersForProduct(productFromDB.ProductIdSimple, message).Result;
-                                Console.WriteLine($"Notified {notifiedUsers} users.");
-                            } catch (Exception ex)
-                            {
-                                Console.WriteLine("Notfifying users failed: \n" +
-                                    $"{ex.Message}");
+                                var message =
+                                    $"{productCurrent.Brand} {productCurrent.Name} now costs {productCurrent.PriceCurrent} {productCurrent.Currency} instead of {productFromDB.PriceCurrent} {productFromDB.Currency}! \n" +
+                                    "\n" +
+                                    $"Here's the link: {productCurrent.Url}" +
+                                    $"\n\n" +
+                                    $"Edit Account Settings: https://www.shoppingtools.online/Identity/Account/Manage";
+
+                                Console.WriteLine(message);
+                                Console.WriteLine("Notifying Users...");
+                                try
+                                {
+                                    var notifiedUsers = UserNotifier.NotifyUsersForProduct(productFromDB.ProductIdSimple, message).Result;
+                                    Console.WriteLine($"Notified {notifiedUsers} users.");
+                                }
+                                catch (Exception ex)
+                                {
+                                    Console.WriteLine("Notfifying users failed: \n" +
+                                        $"{ex.Message}");
+                                }
                             }
-                        }
-                        else
-                        {
-                            Console.WriteLine("Nothing Happened..");
-                        }
-                    });
-                    pendingTasks.Add(task);
-                    task.Start();
-                }
+                            else
+                            {
+                                Console.WriteLine("Nothing Happened..");
+                            }
+                        });
+                        pendingTasks.Add(task);
+                        task.Start();
+                    }
 
-                Console.WriteLine($"Waiting for {pendingTasks.Count} requests to finish..");
-                foreach (var item in pendingTasks)
-                {
-                    item.Wait();
-                }
-                Console.WriteLine("All requests finished!");
+                    Console.WriteLine($"Waiting for {pendingTasks.Count} requests to finish..");
+                    foreach (var item in pendingTasks)
+                    {
+                        item.Wait();
+                    }
+                    Console.WriteLine("All requests finished!");
 
-                if (timer.TimeLeft > 0)
-                {
-                    //sleep the remaining time of the interval
-                    Console.WriteLine($"Sleeping for {TimeSpan.FromMilliseconds(timer.TimeLeft).TotalSeconds} seconds.");
-                    Thread.Sleep(Convert.ToInt32(timer.TimeLeft));
-                }
-                else
-                {
-                    Console.WriteLine($"Updating the database took to long! Now {TimeSpan.FromMilliseconds(timer.TimeLeft).TotalSeconds} seconds behind!");
-                }
+                    if (timer.TimeLeft > 0)
+                    {
+                        //sleep the remaining time of the interval
+                        Console.WriteLine($"Sleeping for {TimeSpan.FromMilliseconds(timer.TimeLeft).TotalSeconds} seconds.");
+                        Thread.Sleep(Convert.ToInt32(timer.TimeLeft));
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Updating the database took to long! Now {TimeSpan.FromMilliseconds(timer.TimeLeft).TotalSeconds} seconds behind!");
+                    }
 
-                //Send Log AFTER Sleep. If the products only take one sec to update, logs will be spammed every second.
-                if (Convert.ToInt32(DateTime.UtcNow.TimeOfDay.TotalHours) == 14)
+                    //Send Log AFTER Sleep. If the products only take one sec to update, logs will be spammed every second.
+                    if (Convert.ToInt32(DateTime.UtcNow.TimeOfDay.TotalHours) == 14)
+                    {
+                        EmailSender.Send("kevin.mueller1@outlook.com", $"Latest updating routine took: {TimeSpan.FromMilliseconds(timer.Interval).TotalMinutes - TimeSpan.FromMilliseconds(timer.TimeLeft).TotalMinutes} minutes", "Daily Updating Routine Log").RunSynchronously();
+                    }
+                }
+                catch (Exception ex)
                 {
-                    EmailSender.Send("kevin.mueller1@outlook.com", $"Latest updating routine took: {TimeSpan.FromMilliseconds(timer.Interval).TotalMinutes - TimeSpan.FromMilliseconds(timer.TimeLeft).TotalMinutes} minutes", "Daily Updating Routine Log").RunSynchronously();
+                    Console.WriteLine($"Internal Error! => {ex.Message}");
                 }
             }
+
 
             Task = null;
         }
