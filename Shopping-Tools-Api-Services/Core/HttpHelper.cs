9b4bd6f0670596c619292902553083ceec50d7b9
Kevin Müller <kevin.mueller1@outlook.com>
2020-02-27 18:27:26 +0100

    added more human request headers

--- Shopping-Tools-Api-Services/Core/HttpHelper.cs
+++ Shopping-Tools-Api-Services/Core/HttpHelper.cs
@@ -23,6 +23,7 @@ namespace Shopping_Tools_Api_Services.Core
             //request.Encoding = Encoding.UTF8;
             request.Method = "GET";
             request.Timeout = 10000;
+            request.Headers = GetHeaderCollection(url);
 
             try
             {
@@ -37,7 +38,8 @@ namespace Shopping_Tools_Api_Services.Core
                 {
                     Console.WriteLine($"Request through proxy successful. URL = {url}");
                     ProxyHelper.GetInstance().TryAddProxyToCache(proxy);
-                } else
+                }
+                else
                 {
                     Console.WriteLine("Request without proxy successful");
                 }
@@ -52,6 +54,25 @@ namespace Shopping_Tools_Api_Services.Core
                 int tmp = failedAttemps += 1;
                 return await GetDocument(url, fastRequest, tmp);
             }
+        }
+
+        private static WebHeaderCollection GetHeaderCollection(string url)
+        {
+            var uri = new Uri(url);
+            return new WebHeaderCollection
+            {
+                { "Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9" },
+                {  "Accept-Encoding", "gzip, deflate, br"},
+                { "Accept-Language", "de-CH,de-DE;q=0.9,de;q=0.8,en-US;q=0.7,en;q=0.6"},
+                { "Cache-Control", "max-age=0"},
+                { "Host", $"{uri.Host}" },
+                { "Referer", "https://www.google.ch"},
+                { "Sec-Fetch-Mode", "navigate"},
+                {"Sec-Fetch-Site", "cross-site" },
+                { "Sec-Fetch-User", "?1"},
+                { "Upgrade-Insecure-Requests", "1" },
+                { "User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.130 Safari/537.36"},
+            };
         }
     }
 }
