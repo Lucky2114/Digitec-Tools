1aa861a46815ba6a3e4953d66997688bf0613c52
Kevin Müller <kevin.mueller1@gmx.ch>
2020-02-29 15:49:56 +0100

    working on implementation of scraperapi

--- Shopping-Tools-Api-Services/Core/HttpHelper.cs
+++ Shopping-Tools-Api-Services/Core/HttpHelper.cs
@@ -8,52 +8,50 @@ namespace Shopping_Tools_Api_Services.Core
 {
     internal static class HttpHelper
     {
-        //TODO Create list of user agents to rotate
-        private const string _userAgent = "Mozilla/5.0 (Windows; U; Windows NT 5.1; de-DE; rv:x.x.x) Gecko/20041107 Firefox/x.x";
-
         internal static async Task<HtmlDocument> GetDocumentAsync(string url, bool fastRequest, int failedAttemps = 0)
         {
             var request = (HttpWebRequest)WebRequest.Create(url);
-            var proxy = ProxyHelper.GetInstance().GetRandomProxy();
-            if (!fastRequest)
-                request.Proxy = proxy;
+            //var proxy = ProxyHelper.GetInstance().GetRandomProxy();
+
+            //if (!fastRequest)
+            //    request.Proxy = proxy;
 
             //request.Headers.Add("User-Agent", _userAgent);
             request.CachePolicy = new RequestCachePolicy(RequestCacheLevel.NoCacheNoStore);
             //request.Encoding = Encoding.UTF8;
             request.Method = "GET";
             request.Timeout = 10000;
-            request.Headers = GetHeaderCollection(url);
+            //request.Headers = GetHeaderCollection(url);
 
-            try
-            {
-                if (failedAttemps > 5)
-                {
-                    Console.WriteLine("Too many failed attemps. Using no proxy.");
-                    request.Proxy = null;
-                }
+            //try
+            //{
+            //    if (failedAttemps > 5)
+            //    {
+            //        Console.WriteLine("Too many failed attemps. Using no proxy.");
+            //        request.Proxy = null;
+            //    }
 
-                var source = await request.GetResponseAsync();
-                if (proxy != null)
-                {
-                    Console.WriteLine($"Request through proxy successful. URL = {url}");
-                    ProxyHelper.GetInstance().TryAddProxyToCache(proxy);
-                }
-                else
-                {
-                    Console.WriteLine("Request without proxy successful");
-                }
-                var doc = new HtmlDocument();
-                doc.Load(source.GetResponseStream());
-                return await Task.FromResult(doc);
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine("Connection through proxy failed. Trying again. \n" +
-                    $"Exception: {ex.Message}");
-                int tmp = failedAttemps++;
-                return await GetDocumentAsync(url, fastRequest, tmp);
-            }
+            //    var source = await request.GetResponseAsync();
+            //    if (proxy != null)
+            //    {
+            //        Console.WriteLine($"Request through proxy successful. URL = {url}");
+            //        ProxyHelper.GetInstance().TryAddProxyToCache(proxy);
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine("Request without proxy successful");
+            //    }
+            //    var doc = new HtmlDocument();
+            //    doc.Load(source.GetResponseStream());
+            //    return await Task.FromResult(doc);
+            //}
+            //catch (Exception ex)
+            //{
+            //    Console.WriteLine("Connection through proxy failed. Trying again. \n" +
+            //        $"Exception: {ex.Message}");
+            //    int tmp = failedAttemps++;
+            //    return await GetDocumentAsync(url, fastRequest, tmp);
+            //}
         }
 
         private static WebHeaderCollection GetHeaderCollection(string url)
